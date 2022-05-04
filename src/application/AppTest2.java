6d212d4620bd8a7fca9930aceb26b749550670ec
Marcos Fernandes <marcosfc.ads@gmail.com>
2022-05-04 08:34:46 -0300

    FindAll department implementation

--- src/application/AppTest2.java
+++ src/application/AppTest2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -10,20 +12,26 @@ public class AppTest2 {
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("#### TEST 1: Insert ####");
+		System.out.println("#### TEST 1: department Insert ####");
 		Department newDepartment = new Department(null,"Music");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! new id = " + newDepartment.getId());
 		
-		System.out.println("\n#### TEST 2: seller sellerUpdate ####");
+		System.out.println("\n#### TEST 2: department  Update ####");
 		Department department = departmentDao.findById(4);
 		department.setName("Corrida");
 		departmentDao.update(department);
 		System.out.println("Update completed");
 		
-		System.out.println("\n#### TEST 3: seller findById ####");
+		System.out.println("\n#### TEST 3: department  findById ####");
 		department = departmentDao.findById(3);		
 		System.out.println(department);
+		
+		System.out.println("\n#### TEST 4: department  findAll ####");
+		List <Department> list = departmentDao.findAll();
+		for(Department obj: list) {
+			System.out.println(obj);
+		}
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -114,7 +117,34 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public List<Department> findAll() {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM department");
+
+			rs = st.executeQuery();
+
+			List<Department> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+
+			while (rs.next()) {
+
+				Department dep = map.get(rs.getInt("Id"));
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("Id"), dep);
+				}
+
+				list.add(dep);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	
 }
